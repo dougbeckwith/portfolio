f4bf74c93180ff0bbe33840c1cb2fe5aa759d52c
Doug Beckwith <dougcbeckwith@gmail.com>
2022-03-28 08:54:07 -0700

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,14 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [isMobileMenu, setIsMobileMenu] = useState(false)
   const handleClick = () => {
-    setNav(!nav)
+    setIsMobileMenu(!isMobileMenu)
   }
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <p className='text-pink-600 text-2xl'>Logo</p>
+        <p className='text-pink-600 text-2xl'>Portfolio</p>
       </div>
       <div>
         {/*Main Nav Links*/}
@@ -44,32 +44,52 @@ const Navbar = () => {
             </Link>
           </li>
           <li>
-          <Link  to="contact" smooth={true} duration={500}>
-          Contact
-        </Link>
+            <Link to='contact' smooth={true} duration={500}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
 
       {/* Mobile Hamburger Icon */}
       <div onClick={handleClick} className='md:hidden z-10'>
-        {/* Display Icon based on Mobile nav state */}
-        {!nav ? <FaBars size={30} /> : <FaTimes size={30} />}
+        {/* Display Icon based on Mobile isMobileMenu state */}
+        {!isMobileMenu ? <FaBars size={30} /> : <FaTimes size={30} />}
       </div>
 
       {/* Mobile Nav Links */}
       <ul
         className={
-          !nav
+          !isMobileMenu
             ? 'hidden'
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Work</li>
-        <li className='py-6 text-4xl'>Contact</li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='projects' smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* Social Icon Side Bar */}
       <div className='hidden xl:flex fixed flex-col top-[35%] left-0'>
