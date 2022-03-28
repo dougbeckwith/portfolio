7819d3957d07306dc17f12cef0f8edfc47b756b1
Doug Beckwith <dougcbeckwith@gmail.com>
2022-03-28 14:36:08 -0700

    added link for projects on View work Button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -19,9 +20,11 @@ const Home = () => {
           web applications.
         </p>
         <div>
-          <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-200'>
-            View Work <HiArrowNarrowRight className='ml-3' />
-          </button>
+          <Link to='projects' smooth={true} duration={500}>
+            <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-200'>
+              View Work <HiArrowNarrowRight className='ml-3' />
+            </button>
+          </Link>
         </div>
       </div>
     </div>
