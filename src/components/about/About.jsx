f0c3849c9e7ea2014f7d5f37c83aaa6ec4efb6e1
Doug Beckwith <dougcbeckwith@gmail.com>
2023-06-01 10:10:37 -0700

    updates to project descriptions

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,71 +1,95 @@
-import React from 'react'
-import {FaNodeJs, FaReact, FaJsSquare, FaGitAlt} from 'react-icons/fa'
-import {SiTailwindcss, SiMongodb, SiExpress} from 'react-icons/si'
+import React from "react";
+import {
+  FaNodeJs,
+  FaReact,
+  FaJsSquare,
+  FaGitAlt,
+  FaHtml5,
+  FaCss3Alt
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiMongodb,
+  SiExpress,
+  SiPostgresql
+} from "react-icons/si";
 
 const About = () => {
   return (
     <div
-      name='about'
-      className='dark:text-gray-500 dark:bg-gray-700 w-full bg-gray-100 py-[100px] md:py-[300px] 2xl:py-[400px] text-gray-600'>
-      <div className='container w-full h-full px-5 mx-auto flex flex-col justify-center items-center'>
-        <section className='text-left w-full'>
-          <h2 className='text-3xl md:text-4xl font-bold inline border-b-4 border-teal-200'>
+      name="about"
+      className="dark:text-gray-500 dark:bg-gray-700 w-full bg-gray-100 py-[100px] md:py-[300px] 2xl:py-[400px] text-gray-600">
+      <div className="container w-full h-full px-5 mx-auto flex flex-col justify-center items-center">
+        <section className="text-left w-full">
+          <h2 className="text-3xl md:text-4xl font-bold inline border-b-4 border-teal-200">
             About
           </h2>
-          <section className='max-w-[1000px]'>
-            <p className='text-lg pt-6 pb-2'>
+          <section className="max-w-[1000px]">
+            <p className="text-lg pt-6 pb-2">
               Web Developer based in BC, Canada.
             </p>
-            <p className='pt-2 pb-4'>
+            <p className="pt-2 pb-4">
               Software Development started out as a hobby for me. I enjoyed it
               so much I decided to make a career change. I’m currently building
               web apps using React and Node.js. Each new project I create, I
               learn something new that improves my developer skills.
             </p>
-            <p className='pt-2 pb-4'>
+            <p className="pt-2 pb-4">
               My favorite project so far is The Halo Infinite project below that
-              I built with another software developer. I would love to share or
-              demo the project for you. If I’m not working, you'll find me
-              golfing or gaming with friends.
+              I built with another software developer. When I'm not working,
+              you'll typically find me either golfing, hitting the gym, or
+              enjoying gaming sessions with my friends.
             </p>
-            <p className='pt-2 pb-2'>
+            <p className="pt-2 pb-2">
               These are the technologies I've worked with.
             </p>
-            <ul className='flex gap-5 flex-wrap text-teal-200'>
-              <div className='flex items-center'>
+            <ul className="flex gap-5 flex-wrap text-teal-200">
+              <div className="flex items-center">
+                <FaHtml5 size={25} />
+                <p className="pl-2">HTML</p>
+              </div>
+              <div className="flex items-center">
+                <FaCss3Alt size={25} />
+                <p className="pl-2">CSS</p>
+              </div>
+              <div className="flex items-center">
                 <FaJsSquare size={25} />
-                <p className='pl-2'>JavaScript</p>
+                <p className="pl-2">JavaScript</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <FaReact size={25} />
-                <p className='pl-2'>React</p>
+                <p className="pl-2">React</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <SiTailwindcss size={25} />
-                <p className='pl-2 w-[140px]'>Tailwind CSS</p>
+                <p className="pl-2 w-[140px]">Tailwind CSS</p>
               </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <FaNodeJs size={25} />
-                <p className='pl-2'>Node.js</p>
+                <p className="pl-2">Node.js</p>
               </div>
-              <div className='flex items-center'>
-                <SiMongodb size={25} />
-                <p className='pl-2'>MongoDB</p>
-              </div>
-              <div className='flex items-center'>
-                <FaGitAlt size={25} />
-                <p className='pl-2'>Git</p>
-              </div>
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <SiExpress size={25} />
-                <p className='pl-2'>Express</p>
+                <p className="pl-2">Express</p>
+              </div>
+              <div className="flex items-center">
+                <SiPostgresql size={25} />
+                <p className="pl-2">PostgreSQL</p>
+              </div>
+              <div className="flex items-center">
+                <SiMongodb size={25} />
+                <p className="pl-2">MongoDB</p>
+              </div>
+              <div className="flex items-center">
+                <FaGitAlt size={25} />
+                <p className="pl-2">Git</p>
               </div>
             </ul>
           </section>
         </section>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/projects/data/projectsData.js
+++ src/components/projects/data/projectsData.js
@@ -23,8 +23,9 @@ const projectData = [
     id: 2,
     image: img2,
     name: "Halo MMR",
-    description: `This app was created to easily help anyone visualize their ranked matchmaking data in Halo Infinite. 
-     It helps solve the problem of understanding your CSR and MMR as the ranking system doesn't give players any feedback. Please ask for demo if you don't play halo and have an account to login.
+    description: `This app was built to help anyone visualize their match making rating and competitive skill rating data in Halo Infinite. 
+    It helps solve the problem of understanding your ratings as the ranking system does not give players good feedback. When the website was live it got around 1000 unique users per day and over 10,000 page loads a day.
+    Although the game has become inactive and we no longer play it, we have taken it down. However, the GitHub repository is still accessible.
     `,
     demo: false,
     demoLink: "https://halommr.com",
@@ -41,21 +42,21 @@ const projectData = [
     name: "Crypto Tracker",
     description: `This web app fetches the top 100 cryptocurrency data from an API. 
     It uses the returned data to display information about each cryptocurrency. You can filter cryptocurrencies,
-    view charts, and make a watchlist. `,
+    view charts, and make a watchlist.`,
     demo: true,
     demoLink: "https://profound-lolly-382242.netlify.app",
     github: true,
     githubLink: "https://github.com/dougbeckwith/crypto-search",
     technologies: ["React", "JavaScript", "Tailwind", "Git"],
     problemsSolutions: `While working on the watchlist feature of the app my watchlist was getting erased anytime I refreshed the webpage. I was able to learn about local storage and store an array of cryptos to repopulate the DOM on refresh. I also felt my code was getting harder to read with chaining .then() methods. So, I learned the async await syntax to make my code more readable.`,
-    lessonsLearned: `I was able to better understand local storage and its use cases. I also learned how to implement different libraries to help with displaying data using charts and spinner icons for when data is loading. I learned about the React Router v6 library for client-side routing.  Made use of useParams() and useNavigate() in React.`
+    lessonsLearned: `I was able to better understand local storage and its use cases. I also learned how to implement different libraries to help with displaying data using charts and spinner icons for when data is loading. I learned about the React Router v6 library for client-side routing.`
   },
 
   {
     id: 4,
     image: img4,
     name: "Golf Stats",
-    description: `I’m Building a full-stack web app that allows users to track their golf club stats. This app uses a RESTful API so users can view create delete update stats on each golf club they own. It uses MongoDB for storing users club data. 
+    description: `I am Building a full-stack web app that allows users to track their golf stats. This app uses a RESTful API so users can view create delete update stats on each golf club they own and more. It uses MongoDB for storing users golf data. 
     `,
     demo: true,
     demoLink: "https://amazing-fenglisu-529860.netlify.app/",
@@ -70,8 +71,8 @@ const projectData = [
       "Tailwind",
       "Git."
     ],
-    problemsSolutions: `While building a more complex app I was having problems writing too much code in once place. I did some research and found out about the MVC design pattern. This allowed me to create a project that was a lot easier to understand and build.  I also had the problem with keeping my local state and the database in sync. After some reading, I noticed Mongoose has a method that returns the object that was just added. I was able to use this object by adding it to my local state to keep it in sync with the database.`,
-    lessonsLearned: `I learned that having a strong project design and feature plan before starting can save development time. I understand now the need to break the project up into multiple sections using The MVC design pattern. I learned how to build my API end points using the RESTful API design.`
+    problemsSolutions: `While building a more complex app I was having problems writing too much code in once place. I did some research and found out about the MVC design pattern. This allowed me to create a project that was a lot easier to understand and build. `,
+    lessonsLearned: `I learned that having a strong project design and feature plan before starting can save development time. I understand now the need to break the project up into multiple sections using The MVC design pattern. I learned how to build my API end points using the RESTful API design. I also had to learn about Authentication and Authorization.`
   }
 ];
 
